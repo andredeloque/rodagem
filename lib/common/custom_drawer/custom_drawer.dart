638d89d09b6a89c5326c63c04a9ef24b7f5d9e80
André Lopes <andredeloque@gmail.com>
2021-06-12 13:54:23 -0300

    correção no drawer

--- lib/common/custom_drawer/custom_drawer.dart
+++ lib/common/custom_drawer/custom_drawer.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rodagem/common/custom_drawer/custom_drawer_header.dart';
 import 'package:rodagem/common/custom_drawer/drawer_tile.dart';
+import 'package:rodagem/models/user_manager.dart';
 
 class CustomDrawer extends StatelessWidget {
   String _idUsuarioLogado;
@@ -29,20 +31,21 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // _recuperarDadosUsuario();
+    //_recuperarDadosUsuario();
     return Drawer(
       child: Stack(
         children: <Widget>[
           Container(
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 203, 236, 241),
-                Colors.white,
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            )),
+              gradient: LinearGradient(
+                colors: [
+                  const Color.fromARGB(255, 203, 236, 241),
+                  Colors.white,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
           ),
           ListView(
             children: <Widget>[
@@ -50,30 +53,40 @@ class CustomDrawer extends StatelessWidget {
               const Divider(),
               DrawerTile(
                 iconData: Icons.home,
-                title: 'Todas reembolsos',
+                title: 'Todos reembolsos',
                 page: 0,
               ),
 
               //if(_typeUser == 'motorista')...[
               //_typeUser == null
 
-              if (_typeUser == "motorista") ...[
-                DrawerTile(
-                  iconData: Icons.admin_panel_settings,
-                  title: 'Meu Perfil',
-                  page: 1,
-                ),
-                DrawerTile(
-                  iconData: Icons.playlist_add_check,
-                  title: 'Cadastrar viagem',
-                  page: 2,
-                ),
-                DrawerTile(
-                  iconData: Icons.car_rental,
-                  title: 'Editar viagens',
-                  page: 3,
-                ),
-              ],
+              Consumer<UserManager>(
+                builder: (_, userManager, __) {
+                  if (userManager.user.typeUser == "motorista") {
+                    return Column(
+                      children: <Widget>[
+                        const Divider(),
+                        DrawerTile(
+                          iconData: Icons.admin_panel_settings,
+                          title: 'Meu Perfil',
+                          page: 1,
+                        ),
+                        DrawerTile(
+                          iconData: Icons.playlist_add_check,
+                          title: 'Cadastrar reembolso',
+                          page: 2,
+                        ),
+                        DrawerTile(
+                          iconData: Icons.car_rental,
+                          title: 'Editar reembolso',
+                          page: 3,
+                        ),
+                      ],
+                    );
+                  }
+                  return Container();
+                },
+              ),
             ],
           ),
         ],
